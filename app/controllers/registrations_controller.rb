161ef59b116c611813e1d98843801385d548f7f0
Nayab Samar <nbsamar@gmail.com>
2019-11-24 00:15:41 +0530

    devise views, modify registration controller

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,3 +1,18 @@
-class RegistrationsController < ApplicationController
+class RegistrationsController < Devise::RegistrationsController
+
+  def index
+
+  end
+
+  def create
+    User.create!(user_create_params)
+  end
+
+  private
+
+  def user_create_params
+    params.require(:user).permit(:first_name, :last_name, :email, :role,
+                                 :password, :password_confirmation)
+  end
 
 end
